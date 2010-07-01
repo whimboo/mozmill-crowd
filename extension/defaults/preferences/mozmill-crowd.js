9a90064dce61d00856295db8f9444fbbf661b8b6
whimboo <hskupin@mozilla.com>
2010-07-01 13:22:50 +0200

    Use real boolean prefs as default values

--- extension/defaults/preferences/mozmill-crowd.js
+++ extension/defaults/preferences/mozmill-crowd.js
@@ -1,6 +1,6 @@
-pref("extensions.mozmill-crowd.report.send", "true");
+pref("extensions.mozmill-crowd.report.send", true);
 pref("extensions.mozmill-crowd.report.server", "http://brasstacks.mozilla.com/couchdb/mozmill/");
 pref("extensions.mozmill-crowd.repositories.mozmill-tests", "https://hg.mozilla.org/qa/mozmill-tests/");
-pref("extensions.mozmill-crowd.repositories.mozmill-tests.custom", "false");
+pref("extensions.mozmill-crowd.repositories.mozmill-tests.custom", false);
 pref("extensions.mozmill-crowd.repositories.mozmill-automation", "https://hg.mozilla.org/qa/mozmill-automation/");
-pref("extensions.mozmill-crowd.trust_unsecure_addons", "false");
+pref("extensions.mozmill-crowd.trust_unsecure_addons", false);
