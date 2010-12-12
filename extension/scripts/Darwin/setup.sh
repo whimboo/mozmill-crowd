126af9ac3d2a0566ae5c0c335adc9aeaf7c81f43
whimboo <hskupin@mozilla.com>
2010-12-11 22:56:28 -0800

    Bug 617768 - Remove left-over IPC-Pipe modules

--- extension/scripts/Darwin/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python mozmill.py $1/env
-source $1/env/bin/activate
-
-easy_install -U mozmill jsbridge mozrunner
-
-hg clone https://hg.mozilla.org/qa/mozmill-automation/
-hg clone https://hg.mozilla.org/qa/mozmill-tests/
--- extension/scripts/Darwin/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# change into the virtual environment and check for updates
-source $1/env/bin/activate
-easy_install -U mozmill jsbridge mozrunner
-
-# start test-run
-python -u $2 $3 $4 $5 $6 $7 $8 $9
