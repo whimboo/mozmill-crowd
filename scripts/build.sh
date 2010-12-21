29184255afcdc7876fe5a13f96549f58d0977290
whimboo <hskupin@mozilla.com>
2010-12-21 02:07:01 +0100

    Remove accidentally added scripts

--- scripts/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://bitbucket.org/ianb/virtualenv/raw/tip/virtualenv.py
-python virtualenv/mac_bootstrap.py mozmill
-rm virtualenv.py
-
-# Move to package folder
-#mv mozmill.py ../extension/resource/scripts
